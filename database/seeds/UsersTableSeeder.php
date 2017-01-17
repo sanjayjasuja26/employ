624ca36ba85997b1bb3db0b5bbab7d29adb11d04
Rajender <rthakur.dev@gmail.com>
2017-01-17 13:14:38 +0530

    create seeder for user migrate auth

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $users = [
+
+        [ 'name'=>'admin','email'=>'admin@gmail.com','password'=>Hash::make('123456')],
+        ['name'=>'superadmin','email'=>'superadmin@gmail.com','password'=>Hash::make('123456')],
+
+        ];
+
+       DB::table('users')->delete();
+       foreach ($users as $user){
+       User::insert($user);
+     }
+
+    }
+}
